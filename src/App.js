0f2fd27f3b08894e04f3fcfdb510106bf2147ea7
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-09-14 07:52:09 -0300

    Feat: Created TextoCentral component

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import TextoCentral from "./components/TextoCentral";
+export default props => {
+    <SafeAreaView>
+        <TextoCentral>App</TextoCentral>
+    </SafeAreaView>
+}
